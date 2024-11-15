c0dc99f2203ac024909cc443b696fa0dc910efd6
Sakshi KC <kcsakshi92@gmail.com>
2024-11-15 22:56:56 +0545

    cors error resolved

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const Routes = require("./Routes/userRoutes");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/", Routes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT} successfully`);
