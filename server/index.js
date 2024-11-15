0c1a4825684d33b9919a993170b8a1a77876f83a
Nirjaraa <nirjarathapa11@gmail.com>
2024-11-16 02:54:13 +0530

    other features added

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use("/", Routes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT} successfully`);
